a88ab95bc8fb4b34a7c9a7e98e33ccad30ec2de9
sakib-git <sakibulhasan32002@gmail.com>
2026-01-17 11:16:16 +0600

    f

--- src/middleware.js
+++ src/middleware.js
@@ -1,20 +1,4 @@
-// import { NextResponse } from 'next/server';
 
-// export function proxy(request) {
-//   const isLoggedIn = request.cookies.get('auth')?.value === 'true';
-
-//   if (!isLoggedIn) {
-//     return NextResponse.redirect(new URL('/login', request.url));
-//   }
-
-//   return NextResponse.next();
-// }
-
-
-// export const config = {
-//   matcher: ["/allproduct", "/allproduct/:path*"],
-// };
-// middleware.js
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
@@ -23,15 +7,15 @@ export function middleware(request) {
 
   // Middleware check
   if (!authCookie || authCookie.value !== "true") {
-    // Redirect to login page
+
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
-  // User logged in, continue
+
   return NextResponse.next();
 }
 
-// Protect /allproduct and nested routes
+
 export const config = {
   matcher: ["/allproduct/:path*"],
 };
